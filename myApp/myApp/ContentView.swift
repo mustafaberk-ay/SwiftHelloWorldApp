eb617cae04d1635c25efe5b3c77cf5a7f7720f2f
mustafaberk01 <70906114+mustafaberk01@users.noreply.github.com>
2021-02-02 00:00:31 +0300

    Add files via upload

---
+++ myApp/myApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  myApp
+//
+//  Created by Mustafa Berk AY on 2021-01-22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ myApp/myApp/myAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  myAppApp.swift
+//  myApp
+//
+//  Created by Mustafa Berk AY on 2021-01-22.
+//
+
+import SwiftUI
+
+@main
+struct myAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
